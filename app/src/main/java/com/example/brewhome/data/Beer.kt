c1832ae99153e010e2e739bc5c126a2d0f831237
Bataklik <60544395+Bataklik@users.noreply.github.com>
2023-11-05 17:20:01 +0100

    feat: :zap: Add more props

--- app/src/main/java/com/example/brewhome/data/Beer.kt
+++ app/src/main/java/com/example/brewhome/data/Beer.kt
@@ -1,8 +1,10 @@
 package com.example.brewhome.data
 
 data class Beer(
-    val id: Int,
+    val id: Int?,
     val name: String,
     val tagline: String,
-    val abv: Double
+    val abv: Double,
+    val image_url: String,
+    val first_brewed:String
 )
