8ec0f56113fff2f71cb145c0bc04acddb9f52ff8
Bataklik <60544395+Bataklik@users.noreply.github.com>
2023-11-05 17:18:29 +0100

    feat: :white_check_mark: Add ViewModel & ViewModelState

--- app/src/main/java/com/example/brewhome/viewmodel/BeerViewModel.kt
+++ app/src/main/java/com/example/brewhome/viewmodel/BeerViewModel.kt
@@ -1,16 +1,20 @@
 package com.example.brewhome.viewmodel
 
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.brewhome.data.Beer
+import com.example.brewhome.data.BeerViewModelState
 import com.example.brewhome.network.BeerRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class BeerViewModel : ViewModel() {
     private val repository = BeerRepository()
-
-    val beersState = mutableStateOf<List<Beer>>(emptyList())
+    private val _beersState = MutableStateFlow(BeerViewModelState(emptyList(), null))
+    val uiState: StateFlow<BeerViewModelState> = _beersState.asStateFlow()
 
     init {
         fetchBeers()
@@ -18,9 +22,22 @@ class BeerViewModel : ViewModel() {
 
     private fun fetchBeers() {
         viewModelScope.launch {
-            val beers = repository.getBeers()
-            if (beers != null) {
-                beersState.value = beers
+            val beers = repository.getBeers() ?: emptyList()
+            _beersState.update { currentState ->
+                currentState.copy(
+                    currentBeers = beers
+                )
+            }
+        }
+    }
+
+    fun getBeerById(beerId: Int) {
+        viewModelScope.launch {
+            val beer = repository.getBeerById(beerId)
+            _beersState.update { currentState ->
+                currentState.copy(
+                    currentBeerById = beer
+                )
             }
         }
     }
