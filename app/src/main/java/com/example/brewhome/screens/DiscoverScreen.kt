c7a3ae1bb4b295b8909eb77a6d531fdb07bdabf8
Bataklik <60544395+Bataklik@users.noreply.github.com>
2023-11-05 17:21:32 +0100

    feat: :fire: Remove hardcoded code, replace with item from API

--- app/src/main/java/com/example/brewhome/screens/DiscoverScreen.kt
+++ app/src/main/java/com/example/brewhome/screens/DiscoverScreen.kt
@@ -7,20 +7,29 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
 import com.example.brewhome.components.BeerCard
+import com.example.brewhome.data.BeerViewModelState
 import com.example.brewhome.viewmodel.BeerViewModel
 
 @Composable
-fun DiscoverScreen(viewModel:BeerViewModel,modifier: Modifier = Modifier) {
-    val beers = viewModel.beersState.value
-    print(beers)
+fun DiscoverScreen(
+    beerViewModel: BeerViewModel,
+    navController: NavController,
+    modifier: Modifier = Modifier
+) {
+    val listState = rememberLazyGridState()
+    val beerViewModelState by beerViewModel.uiState.collectAsState()
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -36,35 +45,17 @@ fun DiscoverScreen(viewModel:BeerViewModel,modifier: Modifier = Modifier) {
         LazyVerticalGrid(
             modifier = Modifier.padding(horizontal = 15.dp),
             columns = GridCells.Adaptive(minSize = 200.dp),
-            verticalArrangement = Arrangement.spacedBy(5.dp)
+            verticalArrangement = Arrangement.spacedBy(5.dp),
+            state = listState
 
         ) {
-            item {
+            items(beerViewModelState.currentBeers) {
                 BeerCard(
-                    name = "Punk IPA 2007 - 2010",
-                    imageUrl = "https://images.punkapi.com/v2/39.png",
-                    tagline = "Post Modern Classic. Spiky. Tropical. Hoppy."
-                )
-            }
-            item {
-                BeerCard(
-                    name = "Black Hammer",
-                    imageUrl = "https://images.punkapi.com/v2/32.png",
-                    tagline = "Our Ruthless IPA With A Dark Malt Twist."
-                )
-            }
-            item {
-                BeerCard(
-                    name = "AB:08",
-                    imageUrl = "https://images.punkapi.com/v2/9.png",
-                    tagline = "Deconstructed Blonde Imperial Stout."
-                )
-            }
-            item {
-                BeerCard(
-                    name = "Kohatu - IPA Is Dead",
-                    imageUrl = "https://images.punkapi.com/v2/2.png",
-                    tagline = "Single Hop India Pale Ale."
+                    navController,
+                    imageUrl = it.image_url,
+                    name = it.name,
+                    tagline = it.tagline,
+                    firstBrewed = it.first_brewed
                 )
             }
         }
