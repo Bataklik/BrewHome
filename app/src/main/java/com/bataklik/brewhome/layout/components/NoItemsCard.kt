2ff6c8a65ed0e20a9c8d26b6b83c13dab5e3852f
Bataklik <60544395+Bataklik@users.noreply.github.com>
2023-12-12 15:02:19 +0100

    feat: :white_check_mark: Update

---
+++ app/src/main/java/com/bataklik/brewhome/layout/components/NoItemsCard.kt
@@ -0,0 +1,69 @@
+package com.bataklik.brewhome.ui.components.favorites
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun NoItemsCard(
+    modifier: Modifier,
+    painterIcon: Painter,
+    titleText: String,
+    descriptionText: String
+) {
+    Column(
+        modifier = modifier
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Card(
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 6.dp
+            ),
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme
+                    .tertiary,
+            ),
+            modifier = modifier
+                .padding(6.dp)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Icon(
+                    painter = painterIcon,
+                    contentDescription = "favoriteIcon"
+                )
+                Column(
+                    modifier = modifier
+                        .padding(horizontal = 10.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = titleText,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = descriptionText,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+        }
+    }
+}
